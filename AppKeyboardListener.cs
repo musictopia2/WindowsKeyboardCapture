1cd52560a5693ff71ac8cba4801abe121cbbb79d
Andy Walter <musictopia2@hotmail.com>
2025-05-23 14:22:02 -0700

    Added new class for handling input when it can only work when i am in focus of app.  Useful for a future app where i hit down arrow to change the highlighting.   however, if i minimize, then should not process arrow down.

---
+++ AppKeyboardListener.cs
@@ -0,0 +1,37 @@
+﻿namespace WindowsKeyboardCapture;
+public class AppKeyboardListener : IDisposable
+{
+    public event Action<EnumKey>? KeyDown;
+    public event Action<EnumKey>? KeyUp;
+    public static Window? MainWindow { get; set; }
+    public AppKeyboardListener()
+    {
+        if (MainWindow == null)
+        {
+            throw new CustomBasicException("MainWindow must be set before using AppKeyboardListener.");
+        }
+        MainWindow.PreviewKeyDown += MainWindow_PreviewKeyDown;
+        MainWindow.PreviewKeyUp += MainWindow_PreviewKeyUp;
+    }
+
+    private void MainWindow_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        KeyDown?.Invoke((EnumKey)KeyInterop.VirtualKeyFromKey(e.Key));
+    }
+
+    private void MainWindow_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        KeyUp?.Invoke((EnumKey)KeyInterop.VirtualKeyFromKey(e.Key));
+    }
+
+    public void Dispose()
+    {
+        if (MainWindow is null)
+        {
+            return;
+        }
+        MainWindow.PreviewKeyDown -= MainWindow_PreviewKeyDown;
+        MainWindow.PreviewKeyUp -= MainWindow_PreviewKeyUp;
+        GC.SuppressFinalize(this); // Added to address CA1816
+    }
+}
--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using CommonBasicLibraries.BasicDataSettingsAndProcesses;
 global using System.Diagnostics;
 global using System.Runtime.InteropServices;
+global using System.Windows;
+global using System.Windows.Input;
